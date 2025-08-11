0b714dea6319f3ee2592710a7f8ac254c17d9ea0
wuwweizn <wuweizhineng@163.com>
2025-08-12 00:47:29 +0800

    替换镜像源

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # wwzn-china
+引用加载项
+官方加载项：https://github.com/home-assistant/addons
+【引用的项目：samb、configurator、matter_server、mosquitto、openwakeword、piper、ssh、whisper】
--- ddnsto/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "启动 DDNSTO 插件..."
-TOKEN=$(jq -r '.token' /data/options.json)
-echo "Token: $TOKEN"
-# 模拟运行
-while true; do
-  echo "运行中..."
-  sleep 60
-done
