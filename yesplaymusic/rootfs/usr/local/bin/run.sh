171b60ca7c5c2dfe422ac19a13ec880abe90e955
wuwweizn <wuweizhineng@163.com>
2025-08-15 04:38:50 +0800

    更新

--- yesplaymusic/rootfs/usr/local/bin/run.sh
+++ yesplaymusic/rootfs/usr/local/bin/run.sh
@@ -5,13 +5,16 @@
 
 set -e
 
+# 等待bashio库加载
+sleep 2
+
 # 读取配置
-NETEASE_API_URL=$(bashio::config 'netease_api_url')
-SSL=$(bashio::config 'ssl')
-CERTFILE=$(bashio::config 'certfile')
-KEYFILE=$(bashio::config 'keyfile')
-CUSTOM_TITLE=$(bashio::config 'custom_title')
-LOG_LEVEL=$(bashio::config 'log_level')
+NETEASE_API_URL=$(bashio::config 'netease_api_url' 'https://music-api.hankqin.com')
+SSL=$(bashio::config 'ssl' 'false')
+CERTFILE=$(bashio::config 'certfile' 'fullchain.pem')
+KEYFILE=$(bashio::config 'keyfile' 'privkey.pem')
+CUSTOM_TITLE=$(bashio::config 'custom_title' 'YesPlayMusic')
+LOG_LEVEL=$(bashio::config 'log_level' 'info')
 
 # 设置日志级别
 bashio::log.level "${LOG_LEVEL}"
@@ -21,6 +24,9 @@ bashio::log.info "API URL: ${NETEASE_API_URL}"
 
 # 设置环境变量
 export VUE_APP_NETEASE_API_URL="/api"
+
+# 创建必要的目录
+mkdir -p /var/log/supervisor /var/log/nginx /run/nginx
 
 # 配置Nginx
 /usr/local/bin/setup_nginx.sh
