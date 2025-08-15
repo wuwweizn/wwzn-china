a6db791ed6af1dc8399f4f7763e78eff1f7f4f14
wuwweizn <wuweizhineng@163.com>
2025-08-15 21:05:23 +0800

    更新

--- passwall/run.sh
+++ passwall/run.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-/etc/init.d/uhttpd start
+
+# 启动 uhttpd (OpenWrt Web 服务器)
+if [ -f /etc/init.d/uhttpd ]; then
+    /etc/init.d/uhttpd start
+fi
+
+# 启动 passwall（如果镜像里可执行）
+if [ -f /usr/bin/passwall ]; then
+    /usr/bin/passwall -f &
+fi
+
+# 保持容器运行
 tail -f /dev/null
