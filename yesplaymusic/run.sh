4eaa80666ee0db570256b5d4fc3ea7e3857a7b60
wuwweizn <wuweizhineng@163.com>
2025-08-16 00:48:58 +0800

    更新

--- yesplaymusic/run.sh
+++ yesplaymusic/run.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# 默认 API 地址，如果 HA 用户在配置里没有改
 API_URL="${API_URL:-http://47.121.211.116:3001/}"
 
 echo "Starting YesPlayMusic with API: $API_URL"
 
-# 启动 YesPlayMusic
-npm start -- --port 3000 --api "$API_URL"
+# 启动生产构建后的服务
+npm run serve -- --port 3000 --api "$API_URL"
