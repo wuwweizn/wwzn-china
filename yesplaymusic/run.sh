9eb59c3385fd65373a4f8aeaaaf5ce6c23e78b79
wuwweizn <wuweizhineng@163.com>
2025-08-16 01:10:18 +0800

    更新

--- yesplaymusic/run.sh
+++ yesplaymusic/run.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-API_URL="${API_URL:-http://47.121.211.116:3001/}"
 echo "Starting YesPlayMusic with API: $API_URL"
-
-# 启动编译好的版本
-npm install -g serve
-serve -s dist -l 3000
+# 如果镜像支持 API_URL 环境变量，可以直接运行
+docker-entrypoint.sh
