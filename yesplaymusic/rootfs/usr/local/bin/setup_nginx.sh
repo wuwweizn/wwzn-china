be1d78611f01c5767f597cca922eca71147e3caf
wuwweizn <wuweizhineng@163.com>
2025-08-18 20:54:06 +0800

    更新

--- yesplaymusic/rootfs/usr/local/bin/setup_nginx.sh
+++
@@ -1,5 +0,0 @@
-# =============================================================================
-# rootfs/usr/local/bin/setup_nginx.sh (可选，现在已内嵌到Dockerfile)
-# =============================================================================
-#!/bin/bash
-echo "Nginx setup is now handled in the main startup script."
