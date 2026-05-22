41a6f6436c74a31c58d25b77001672d4c149e905
liyong763435720 <98336989+liyong763435720@users.noreply.github.com>
2026-05-22 18:08:15 +0800

    更新

--- rtp2httpd/run.sh
+++ rtp2httpd/run.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/with-contenv bashio
 
-# 读取 HA 配置
+bashio::log.info "========================================="
+bashio::log.info "  rtp2httpd 启动中..."
+bashio::log.info "========================================="
+
+# 读取加载项配置
 LISTEN_PORT=$(bashio::config 'listen_port')
 MAX_CLIENTS=$(bashio::config 'max_clients')
 VERBOSE=$(bashio::config 'verbose')
 EXTRA_ARGS=$(bashio::config 'extra_args')
 
-bashio::log.info "Starting rtp2httpd on port ${LISTEN_PORT}..."
-bashio::log.info "Max clients: ${MAX_CLIENTS}, Verbose level: ${VERBOSE}"
+bashio::log.info "监听端口: ${LISTEN_PORT}"
+bashio::log.info "最大客户端数: ${MAX_CLIENTS}"
+bashio::log.info "日志级别: ${VERBOSE}"
 
-# 检查是否有挂载的配置文件
-if bashio::fs.file_exists "/config/rtp2httpd.conf"; then
-    bashio::log.info "Using config file: /config/rtp2httpd.conf"
+# 检查用户是否在 addon_config 目录放了配置文件
+CONFIG_FILE="/config/rtp2httpd.conf"
+
+if bashio::fs.file_exists "${CONFIG_FILE}"; then
+    bashio::log.info "发现配置文件: ${CONFIG_FILE}，使用配置文件模式启动"
     exec /usr/local/bin/rtp2httpd \
-        --config /config/rtp2httpd.conf \
+        --config "${CONFIG_FILE}" \
         ${EXTRA_ARGS}
 else
-    bashio::log.info "No config file found, using command line arguments"
+    bashio::log.info "未发现配置文件，使用参数模式启动"
+    bashio::log.info "提示: 可在 /addon_configs/rtp2httpd/ 目录下放置 rtp2httpd.conf 以使用配置文件"
     exec /usr/local/bin/rtp2httpd \
         --noconfig \
         --verbose "${VERBOSE}" \
